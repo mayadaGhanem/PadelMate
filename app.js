4842238c20a195e063cb775441ea4028c66343d9
nour-gamal <nmg181963@hotmail.com>
2024-07-07 13:44:13 +0300

    initial Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+var express = require("express");
+//var cors = require("cors");
+var app = express();
+// app.use(cors);
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = process.env.DB_URL;
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {s
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server (optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     const db= await client.db('padel_mate');
+
+//   const res= await db.collection("users").find().toArray();
+//     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//     console.log(res,'res');
+//   }catch(e) {
+//     // Ensures that the client will close when you finish/error
+//     console.log(e,'error')
+//   } finally {
+//     // Ensures that the client will clßose when you finish/error
+//     await client.close();
+//   }
+// }
+// run()
+var server = app.listen(5001, function () {
+	console.log("Express App running at http://127.0.0.1:5001/");
+});
